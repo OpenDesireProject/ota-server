9fd3d10baac8d323c5d716341d4113bbec945279
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-04-09 20:11:49 +0000

    update SQL structure

--- create_database.sql
+++ create_database.sql
@@ -1,8 +1,4 @@
---
--- Table structure for table `updates`
---
-
-CREATE TABLE `updates` (
+CREATE TABLE IF NOT EXISTS `updates` (
   `filename` varchar(255) NOT NULL,
   `device` varchar(16) NOT NULL,
   `incremental` char(10) NOT NULL,
@@ -11,11 +7,7 @@ CREATE TABLE `updates` (
   `channel` varchar(16) NOT NULL,
   `api_level` tinyint(4) NOT NULL,
   `url` varchar(255) NOT NULL,
-  `changes` varchar(255) NOT NULL
+  `changes` varchar(255) NOT NULL,
+  `mirror_id` tinyint(4) NOT NULL,
+  PRIMARY KEY (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for table `updates`
---
-ALTER TABLE `updates`
- ADD PRIMARY KEY (`filename`);
